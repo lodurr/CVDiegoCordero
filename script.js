712042e2cf953c09428ebeed06b4a4d7f5fce9fb
Diego Cordero <ing.diegocordero@gmail.com>
2022-02-07 20:18:45 -0400

    CV Javascript
    CV Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+//Funcion para el boton
+function descargarResume() {
+    window.open("ResumeDiegoCorderoEs22.pdf");
+}
+//Funcion para el texto Nombre
+function downloadResume() {
+    window.open("ResumeDiegoCorderoEng.pdf");
+}
+
+function enviarCorreo() {
+    window.open('mailto:ing.diegocordero@gmail.com?subject=Solicitud de Contacto');
+}
